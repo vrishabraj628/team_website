6f201c6cde6ac1730b4a532b212f8b4473193ec8
vrishabraj628 <vrishabgtk@gmail.com>
2025-02-25 21:07:59 +0530

    retry changes

--- nav-bar.js
+++ nav-bar.js
@@ -6,3 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
     hamburger.classList.toggle('active');
   });
 });
+
+// For Navigation Hamburger
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+const closeMenu = document.querySelector(".close");
+const body = document.querySelector("body");
+const vw = window.innerWidth;
+if (closeMenu.style.display === "flex") {
+  closeMenu.style.display = "none";
+  body.classList.remove("no-scroll");
+} else {
+  closeMenu.style.display = "flex";
+  body.classList.add("no-scroll");
+  if (vw <= 430){
+    closeMenu.style.width = "100vw";
+  }
+  else{
+    closeMenu.style.width = "20vw";
+  }
+}
+});
